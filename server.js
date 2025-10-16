596615db0b4c2a80525a5508a2bbec66f0ed9b8f
Mohmed Aasim Shaikh <aasim26112005@gmail.com>
2025-10-16 21:33:40 +0530

    Initial commit of chat app

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: "*" } });
+
+// serve index.html on root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+io.on("connection", socket => {
+  console.log("User connected:", socket.id);
+
+  socket.on("chatMessage", msg => {
+    io.emit("chatMessage", msg);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+server.listen(3000, () => console.log("✅ Server running on http://localhost:3000"));
